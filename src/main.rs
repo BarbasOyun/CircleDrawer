e198991f099ae122ae54094acd912d50543d8976
barbasoyun <barbasoyun@gmail.com>
2026-04-07 20:12:05 +0200

    WASM Version

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 mod gears;
 
-use eframe::{egui::*, wgpu::wgc::device::global};
+use eframe::egui::*;
 
+#[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result {
     let mut circle_drawer = CircleDrawer::new();
 
@@ -10,16 +12,65 @@ fn main() -> eframe::Result {
     circle_drawer.create_circle(vec2(450.0, 300.0), 150.0, 20, Color32::GREEN, 2.0);
     circle_drawer.create_circle(vec2(500.0, 300.0), 200.0, 20, Color32::BLUE, 2.0);
 
-    return start_app(circle_drawer);
-}
-
-fn start_app<T: eframe::App + 'static>(app: T) -> eframe::Result {
     let options = eframe::NativeOptions {
         viewport: ViewportBuilder::default().with_inner_size([800.0, 600.0]),
         ..Default::default()
     };
 
-    eframe::run_native("Circle Drawer", options, Box::new(|_cc| Ok(Box::new(app))))
+    return eframe::run_native("Circle Drawer", options, Box::new(|_cc| Ok(Box::new(circle_drawer))))
+}
+
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    use eframe::wasm_bindgen::JsCast as _;
+
+    let mut circle_drawer = CircleDrawer::new();
+
+    // Draw Default Circles
+    circle_drawer.create_circle(vec2(400.0, 300.0), 100.0, 20, Color32::RED, 2.0);
+    circle_drawer.create_circle(vec2(450.0, 300.0), 150.0, 20, Color32::GREEN, 2.0);
+    circle_drawer.create_circle(vec2(500.0, 300.0), 200.0, 20, Color32::BLUE, 2.0);
+
+    // Redirect `log` message to `console.log` and friends:
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        let document = web_sys::window()
+            .expect("No window")
+            .document()
+            .expect("No document");
+
+        let canvas = document
+            .get_element_by_id("the_canvas_id")
+            .expect("Failed to find the_canvas_id")
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .expect("the_canvas_id was not a HtmlCanvasElement");
+
+        let start_result = eframe::WebRunner::new()
+            .start(
+                canvas,
+                web_options,
+                Box::new(|cc| Ok(Box::new(circle_drawer))),
+            )
+            .await;
+
+        // Remove the loading text and spinner:
+        if let Some(loading_text) = document.get_element_by_id("loading_text") {
+            match start_result {
+                Ok(_) => {
+                    loading_text.remove();
+                }
+                Err(e) => {
+                    loading_text.set_inner_html(
+                        "<p> The app has crashed. See the developer console for details. </p>",
+                    );
+                    panic!("Failed to start eframe: {e:?}");
+                }
+            }
+        }
+    });
 }
 
 struct CircleDrawer {
@@ -175,7 +226,6 @@ impl eframe::App for CircleDrawer {
 
             // Create Circle on Click
             if response.hovered() && left_mouse_button_released {
-
                 self.create_circle(
                     vec2(global_mouse_position.x, global_mouse_position.y),
                     self.radius,
